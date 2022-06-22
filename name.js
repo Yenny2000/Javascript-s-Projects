743540a69ac47a94eed92cfa7d1ba5df3a8bfee4
Yenny2000 <105251676+Yenny2000@users.noreply.github.com>
2022-06-22 03:23:53 +0100

    Add files via upload

---
+++ name.js
@@ -0,0 +1,13 @@
+const User ="Omoyeni Omikunle";
+document.write ("Name: " + User);
+document.write ("<br>");
+const age = "22";
+document.write ("Age: " + age);
+document.write ("<br>");
+const height = "5'4";
+document.write ("Height: " + height);
+document.write ("<br>");
+const country ="Nigeria";
+document.write ("Country: " + country);
+
+
